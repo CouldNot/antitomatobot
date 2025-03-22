372bebf7b7c5d971919633e2979b3f4442a77908
Dale Dai <ddale1128@gmail.com>
2025-03-21 22:27:10 -0700

    Seperate firebase init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { collection, doc, getDoc, setDoc, getDocs } from "firebase/firestore";
 import {
   Client,
@@ -12,23 +10,11 @@ import { OpenAI } from "openai";
 import "dotenv/config";
 import cron from "node-cron";
 import moment from "moment-timezone";
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
+import db from "./services/firebase";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_PROJECT_KEY,
 });
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 const client = new Client({
   intents: [
