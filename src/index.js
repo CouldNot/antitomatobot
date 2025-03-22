f9d29c0dbd4dbfb6cce9c479e03b0cc4390421b5
Dale Dai <ddale1128@gmail.com>
2025-03-21 22:01:26 -0700

    adjust prompt again x2

--- src/index.js
+++ src/index.js
@@ -627,7 +627,7 @@ client.on("interactionCreate", async (interaction) => {
   }
 
   // Format messages for OpenAI
-  const prompt = `Summarize the following online conversation casually, mimicking the language used. Note that the messages towards the left (start) are newest, and that messages towards the end of the following text are oldest: \n\n ${messagesToSummarize.join(
+  const prompt = `Summarize the following online conversation casually, mimicking the language used. Note that the messages towards the left (start) are newest, and that messages towards the end of the following text are oldest. So if you were to summarize chronologically, you would go from END to START: \n\n ${messagesToSummarize.join(
     "\n"
   )}`;
 
